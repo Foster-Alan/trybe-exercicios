b54cbafc1342d734cc38071c7fbfde7285b24364
Alan Foster <alan-af@outlook.com>
2022-06-24 19:25:14 -0300

    exercicios terminados

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/8.js
@@ -0,0 +1,15 @@
+// Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+ 
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+  console.log(leitor.nome + ' tem 2 livros Favoritos');
