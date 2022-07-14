9d79f1c9f1083385f031a59955228c37b79cddaa
Alan Foster <alan-af@outlook.com>
2022-07-14 18:44:53 -0300

    primeiros exercicios resolvidos

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,45 @@
+//   //🚀 Altere as variáveis para respeitarem o escopo em que foram declaradas.
+// Modifique a estrutura da função para que ela seja uma arrow function;
+// Modifique as variáveis para que respeitem o escopo onde estão declaradas;
+// Modifique as concatenações para template literals. //
+
+
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    
+  }
+
+  testingScope(true);
+
+
+//   🚀 Crie uma função que retorne um array em ordem crescente.
+// Crie uma função que faça com que o array oddsAndEvens fique na ordem crescente;
+// Utilize o console.log e template literals para retornar a frase: 'Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!'.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne a frase "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const orderOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens;
+};
+const numeroEmOrdem = orderOddsAndEvens();
+
+console.log(`Os números ${numeroEmOrdem} se encontram ordenados de forma crescente!`);
+
+
