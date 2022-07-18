70d22cf4e11e64842af20f052afe84eb845c36c9
Alan Foster <alan-af@outlook.com>
2022-07-18 19:14:40 -0300

    sum test feito

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('./sum.js');
+
+describe('1', () => {
+    it('teste o retorno de sum(4, 5) é igual a 9', () => {
+        expect(sum(4, 5)).toBe(9);
+    });
+    it('teste se o retorno de sum(0, 0) é 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    });
+    it('teste se a função sum lança um erro quando os paramtros sao 4 e "5"', () =>{
+        expect(() => sum(4, '5')).toThrow(Error);
+    })
+});
